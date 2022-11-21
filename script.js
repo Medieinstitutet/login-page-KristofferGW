a42eadf0b688dbf85707a2d15ea7ba552d01a18b
Kristoffer Wallqvist <112932893+KristofferGW@users.noreply.github.com>
2022-11-21 14:46:15 +0100

    Janne upplagd som user

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 document.body.style.backgroundColor = "black";
 document.body.style.color = "white";
+
+const userNameField = document.getElementById("userNameField");
+const passwordField = document.getElementById("passwordField");
+const logInBtn = document.getElementById("logInBtn");
+
+users = [
+    {"name": "Janne", "password": "test"}
+]
